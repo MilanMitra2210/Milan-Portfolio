20e1c67eb5b61575d7ae205c94f8af565b81c1f2
MilanMItra2210 <milanmitranda2020@gmail.com>
2024-06-27 22:26:11 +0530

    icon update

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -50,12 +50,12 @@ const ProjectCard = ({
         <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
           <div
             onClick={() => window.open(source_code_link, "_blank")}
-            className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+            className='black-gradient w-14 h-14 rounded-full flex justify-center items-center cursor-pointer'
           >
             <img
               src={github}
               alt='source code'
-              className='w-1/2 h-1/2 object-contain'
+              className='w-5/6 h-5/6 object-contain'
             />
           </div>
         </div>
